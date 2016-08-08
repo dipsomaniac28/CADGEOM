aeff94b0eed82b6551a3e46399ff14f62640ab92
dipsomaniac28 <brianwillis28@gmail.com>
2016-08-08 14:58:49 +0200

    test

---
+++ firstDump.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+###
+### This file is generated automatically by SALOME v7.8.0 with dump python functionality
+###
+
+import sys
+import salome
+
+salome.salome_init()
+theStudy = salome.myStudy
+
+import salome_notebook
+notebook = salome_notebook.NoteBook(theStudy)
+sys.path.insert( 0, r'/home/brianwillis/CAD/Geometries')
+
+###
+### GEOM component
+###
+
+import GEOM
+from salome.geom import geomBuilder
+import math
+import SALOMEDS
+
+
+geompy = geomBuilder.New(theStudy)
+
+O = geompy.MakeVertex(0, 0, 0)
+OX = geompy.MakeVectorDXDYDZ(1, 0, 0)
+OY = geompy.MakeVectorDXDYDZ(0, 1, 0)
+OZ = geompy.MakeVectorDXDYDZ(0, 0, 1)
+Vertex_1 = geompy.MakeVertex(1, 0, 0)
+Vertex_2 = geompy.MakeVertex(1, 1, 0)
+Vertex_3 = geompy.MakeVertex(0, 1, 0)
+Arc_1 = geompy.MakeArc(Vertex_3, Vertex_2, Vertex_1)
+geompy.addToStudy( O, 'O' )
+geompy.addToStudy( OX, 'OX' )
+geompy.addToStudy( OY, 'OY' )
+geompy.addToStudy( OZ, 'OZ' )
+geompy.addToStudy( Vertex_1, 'Vertex_1' )
+geompy.addToStudy( Vertex_2, 'Vertex_2' )
+geompy.addToStudy( Vertex_3, 'Vertex_3' )
+geompy.addToStudy( Arc_1, 'Arc_1' )
+
+
+if salome.sg.hasDesktop():
+  salome.sg.updateObjBrowser(1)
